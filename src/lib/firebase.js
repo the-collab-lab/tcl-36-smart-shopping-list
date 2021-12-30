e87de1559aa3040bc93ac8db22b2ed0cf080aeee
stacie.taylor <stacie.taylor@zapier.com>
2021-12-30 11:54:38 -0800

    Update firebase keys

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -5,12 +5,12 @@ import { getFirestore } from 'firebase/firestore';
 // Initialize Firebase.
 // These details will need to be replaced with the project specific env vars at the start of each new cohort.
 const firebaseConfig = {
-  apiKey: API_KEY,
-  authDomain: AUTH_DOMAIN,
-  projectId: PROJECT_ID,
-  storageBucket: STORAGE_BUCKET,
-  messagingSenderId: MESSAGING_SENDER_ID,
-  appId: APP_ID
+  apiKey: "AIzaSyDVqB8ngR5YlYY2VQhWCrPfvORMTHz3ddA",
+  authDomain: "tcl-36-smart-shopping-list.firebaseapp.com",
+  projectId: "tcl-36-smart-shopping-list",
+  storageBucket: "tcl-36-smart-shopping-list.appspot.com",
+  messagingSenderId: "423140826190",
+  appId: "1:423140826190:web:13a659310e2c8b80eab71f"
 };
 
 export const app = initializeApp(firebaseConfig);
