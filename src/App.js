a21f004f17393b7a1984ea98e605660b341526fc
LindseyinTech <lindseyintech@gmail.com>
2022-01-11 14:28:03 -0800

    added button, input, useState, firebase

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [click, setClick] = useState(false);
+  const [text, setText] = useState('');
+
+  //updating state for the button
+  function handleClick(e) {
+    e.preventDefault();
+    setClick(!click);
+  }
+  //updating state for the input box
+  function handleChange(e) {
+    setText(e.target.value);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="temp">
+        <form method="post">
+          <input
+            type="text"
+            placeholder="Enter Text"
+            name="text"
+            onChange={(e) => handleChange(e)}
+          ></input>
+          <button type="submit" onClick={(e) => handleClick(e)}>
+            Click this button to send data!
+          </button>
+        </form>
+        <div>{text}</div>
+      </div>
     </div>
   );
 }
