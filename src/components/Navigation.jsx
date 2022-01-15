23b421a624246ac31c9f2b101c39beb5b4db9457
Alena <83471057+alenamedved@users.noreply.github.com>
2022-01-14 21:47:45 -0700

    add css to navbar

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,16 +1,24 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
+const navStyles = {
+  position: 'fixed',
+  bottom: '0',
+  width: '100%',
+  textAlign: 'center',
+  backgroundColor: '#fff',
+};
+
 export default function Navigation() {
   return (
-    <nav>
+    <nav style={navStyles}>
       <NavLink
         to="/addItemView"
         style={({ isActive }) => ({ fontWeight: isActive ? '800' : '' })}
       >
         Add an Item
       </NavLink>
-
+      &nbsp;
       <NavLink
         to="/listView"
         style={({ isActive }) => ({ fontWeight: isActive ? '800' : '' })}
