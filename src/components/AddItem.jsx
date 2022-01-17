73e9d74bb354b9f0adeaf3be7e93cb586df7f96d
LindseyinTech <lindseyintech@gmail.com>
2022-01-17 12:11:17 -0800

    added form with radio buttons and text input /AddItem.jsx

--- src/components/AddItem.jsx
+++ src/components/AddItem.jsx
@@ -1,7 +1,38 @@
 import React from 'react';
 
 function AddItem() {
-  return <div>Add Item</div>;
+  return (
+    <form>
+      <label htmlFor="itemName">Item Name:</label>
+      <input type="text" id="itemName" name="itemName"></input>
+
+      <fieldset>
+        <legend>Choose how soon you will buy this again </legend>
+        <input
+          type="radio"
+          id="soon"
+          name="frequency"
+          value="soon"
+          checked
+        ></input>
+        <label htmlFor="soon">soon</label>
+        <input
+          type="radio"
+          id="kind of soon"
+          name="frequency"
+          value="kind of soon"
+        ></input>
+        <label htmlFor="kind of soon">kind of soon</label>
+        <input
+          type="radio"
+          id="not soon"
+          name="frequency"
+          value="not soon"
+        ></input>
+        <label htmlFor="not soon">not soon</label>
+      </fieldset>
+    </form>
+  );
 }
 
 export default AddItem;
