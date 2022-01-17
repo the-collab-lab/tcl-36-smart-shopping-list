c56d620b7355c18825a477b371a9ccc268063002
LindseyinTech <lindseyintech@gmail.com>
2022-01-17 13:21:14 -0800

    radio button functionalities, sending data to fb

--- src/components/AddItem.jsx
+++ src/components/AddItem.jsx
@@ -1,37 +1,96 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import { collection, addDoc, onSnapshot } from 'firebase/firestore';
+import { db } from '../lib/firebase';
 
 function AddItem() {
-  return (
-    <form>
-      <label htmlFor="itemName">Item Name:</label>
-      <input type="text" id="itemName" name="itemName"></input>
+  const [itemName, setItemName] = useState('');
+  const [frequency, setFrequency] = useState(7);
+  const [notification, setNotification] = useState('');
 
-      <fieldset>
-        <legend>Choose how soon you will buy this again </legend>
+  //handle submit for the form
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    //create new doc reference for text input and save to firestore database
+    //at later point will want to change collection(db, "items") to collecton(db, token) to send items to a specific collection separated by token names
+    try {
+      const docRef = await addDoc(collection(db, 'items'), {
+        //data points being sent to firebase, object format
+        itemName: itemName,
+        frequency: Number(frequency),
+        purchasedDate: null,
+      });
+      setNotification(`Successfully added ${itemName}`);
+      setItemName('');
+      console.log('Document written with ID: ', docRef.id);
+      //setTimeout is used to clear the success notification after 2 seconds
+      setTimeout(() => {
+        setNotification('');
+      }, 2000);
+    } catch (e) {
+      console.error('Error adding document: ', e);
+    }
+  };
+  //updating state for the input box
+  function handleChange(e) {
+    setItemName(e.target.value);
+  }
+
+  //tracks radio button value = frequency
+  function handleFrequencyChange(e) {
+    setFrequency(e.target.value);
+  }
+
+  return (
+    <div>
+      <br></br>
+      {notification}
+      <br></br>
+      <form method="post">
+        <label htmlFor="itemName">Item Name:</label>
         <input
-          type="radio"
-          id="soon"
-          name="frequency"
-          value="soon"
-          checked
+          type="text"
+          id="itemName"
+          name="itemName"
+          value={itemName}
+          onChange={(e) => handleChange(e)}
         ></input>
-        <label htmlFor="soon">soon</label>
-        <input
-          type="radio"
-          id="kind of soon"
-          name="frequency"
-          value="kind of soon"
-        ></input>
-        <label htmlFor="kind of soon">kind of soon</label>
-        <input
-          type="radio"
-          id="not soon"
-          name="frequency"
-          value="not soon"
-        ></input>
-        <label htmlFor="not soon">not soon</label>
-      </fieldset>
-    </form>
+        <fieldset>
+          <legend>Choose how soon you will buy this again</legend>
+          <input
+            type="radio"
+            id="soon"
+            name="frequency"
+            value={7}
+            onChange={(e) => handleFrequencyChange(e)}
+            defaultChecked
+          ></input>
+          <label htmlFor="soon">soon</label>
+          <br></br>
+          <input
+            type="radio"
+            id="kind of soon"
+            name="frequency"
+            value={14}
+            onChange={(e) => handleFrequencyChange(e)}
+          ></input>
+          <label htmlFor="kind of soon">kind of soon</label>
+          <br></br>
+          <input
+            type="radio"
+            id="not soon"
+            name="frequency"
+            value={30}
+            onChange={(e) => handleFrequencyChange(e)}
+          ></input>
+          <label htmlFor="not soon">not soon</label>
+          <br></br>
+        </fieldset>
+        <button type="submit" onClick={(e) => handleSubmit(e)}>
+          Add Item
+        </button>
+      </form>
+    </div>
   );
 }
 
--- src/components/ListView.jsx
+++ src/components/ListView.jsx
@@ -1,10 +1,9 @@
 import React, { useEffect, useState } from 'react';
 
-import { collection, addDoc, onSnapshot } from 'firebase/firestore';
+import { collection, onSnapshot } from 'firebase/firestore';
 import { db } from '../lib/firebase';
 
 function ListView() {
-  const [itemText, setItemText] = useState('');
   const [items, setItems] = useState([]);
 
   //use effect enables the app to listen for changes to the database and updates the state accordingly
@@ -20,47 +19,17 @@ function ListView() {
     };
   }, []);
 
-  //handle submit for the form
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    //create new doc reference for text input and save to firestore database
-    try {
-      const docRef = await addDoc(collection(db, 'items'), {
-        itemText,
-      });
-      setItemText('');
-      console.log('Document written with ID: ', docRef.id);
-    } catch (e) {
-      console.error('Error adding document: ', e);
-    }
-  };
-  //updating state for the input box
-  function handleChange(e) {
-    setItemText(e.target.value);
-  }
-
   return (
     <div>
       List View
       <div className="App">
-        <div>
-          <form method="post">
-            <input
-              type="text"
-              placeholder="New item"
-              name="item"
-              value={itemText}
-              onChange={(e) => handleChange(e)}
-            />
-            <button type="submit" onClick={(e) => handleSubmit(e)}>
-              Click this button to send data!
-            </button>
-          </form>
+        <ul>
           {items.map((item, idx) => (
-            <div key={idx}>{item.itemText}</div>
+            <li
+              key={idx}
+            >{` Name: ${item.itemName}  Frequency: ${item.frequency}`}</li>
           ))}
-        </div>
+        </ul>
       </div>
     </div>
   );
