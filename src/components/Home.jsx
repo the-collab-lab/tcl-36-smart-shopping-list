9efbbfd2bc3fd2100d4eb39fbfd2e591c8bddd61
DJ <dtaylor88@gmail.com>
2022-01-17 14:15:43 -0800

    add getToken package, create button for new lists and redirect to list view

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,38 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import { collection, addDoc } from 'firebase/firestore';
+import { db } from '../lib/firebase';
+
+import { getToken } from '@the-collab-lab/shopping-list-utils';
 
 function Home() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const checkToken = localStorage.getItem('list-token');
+    if (checkToken) navigate('/listView');
+  });
+
+  const handleNewList = async () => {
+    const token = getToken();
+    localStorage.setItem('list-token', token);
+    try {
+      const docRef = await addDoc(collection(db, 'token'), {
+        token,
+      });
+      navigate('/listView');
+      console.log('Document written with ID: ', docRef.id);
+    } catch (e) {
+      console.error('Error adding document: ', e);
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <p>Our Shopping App</p>
+        <button onClick={() => handleNewList()}>Create New List</button>
       </header>
     </div>
   );
