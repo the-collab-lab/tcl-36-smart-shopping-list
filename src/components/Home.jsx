c3e47fa285ae3467c24c7330e1139fedb828049f
DJ <dtaylor88@gmail.com>
2022-01-25 12:11:27 -0800

    add handle token submit function, check if it exists in local storage and firebase  or show error

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,15 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-import { collection, addDoc } from 'firebase/firestore';
+import { collection, addDoc, getDocs, query, where } from 'firebase/firestore';
 import { db } from '../lib/firebase';
 
 import { getToken } from '@the-collab-lab/shopping-list-utils';
 
 function Home() {
+  const [userToken, setUserToken] = useState('');
+  const [notification, setNotification] = useState('');
+
   const navigate = useNavigate();
 
   //check for existing token and go to List View if it is there
@@ -31,11 +34,53 @@ function Home() {
     }
   };
 
+  const handleTokenSubmit = async () => {
+    const q = query(collection(db, 'token'), where('token', '==', userToken));
+
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+      localStorage.setItem('list-token', doc.data().token);
+    });
+    const localToken = localStorage.getItem('list-token');
+    if (localToken) {
+      navigate('/listView');
+    } else {
+      setNotification(
+        'the token does not exist. try again or create a new list',
+      );
+      //setTimeout is used to clear the success notification after 2 seconds
+      setTimeout(() => {
+        setNotification('');
+      }, 2000);
+      console.log('this token does not exist...');
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <p>Our Shopping App</p>
         <button onClick={() => handleNewList()}>Create New List</button>
+        <div>
+          <p>--Or--</p>
+          <p>Join Existing List by Entering Three Word Token</p>
+        </div>
+        {notification}
+        <label htmlFor="userToken">List Token:</label>
+        <input
+          type="text"
+          id="userToken"
+          name="userToken"
+          value={userToken}
+          onChange={(e) => setUserToken(e.target.value)}
+        ></input>
+        <button
+          type="submit"
+          disabled={!userToken} //button is disabled until user input an item name
+          onClick={(e) => handleTokenSubmit(e)}
+        >
+          Add Item
+        </button>
       </header>
     </div>
   );
