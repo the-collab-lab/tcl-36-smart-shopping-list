70eda9c0c6c922bfcb8995996230ee20870ad1a8
LindseyinTech <lindseyintech@gmail.com>
2022-01-28 21:21:09 -0800

    reformatted Errors.js

--- src/components/Errors.jsx
+++ src/components/Errors.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function Errors({ message }) {
-  return <div>{<p className="error">Error: {message}</p>}</div>;
+  return <div className="error">Error: {message}</div>;
 }
 
 export default Errors;
