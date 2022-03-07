60716c31a2f3bad7118050e96c4ffd11e5529578
Alena <83471057+alenamedved@users.noreply.github.com>
2022-03-07 10:41:09 -0700

    new web app's Firebase configuration

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -5,12 +5,12 @@ import { getFirestore, updateDoc, doc, deleteDoc } from 'firebase/firestore';
 // Initialize Firebase.
 // These details will need to be replaced with the project specific env vars at the start of each new cohort.
 const firebaseConfig = {
-  apiKey: 'AIzaSyDVqB8ngR5YlYY2VQhWCrPfvORMTHz3ddA',
-  authDomain: 'tcl-36-smart-shopping-list.firebaseapp.com',
-  projectId: 'tcl-36-smart-shopping-list',
-  storageBucket: 'tcl-36-smart-shopping-list.appspot.com',
-  messagingSenderId: '423140826190',
-  appId: '1:423140826190:web:13a659310e2c8b80eab71f',
+  apiKey: 'AIzaSyAYwyfvPer1Z7KLemz8Xi7Z4wmFr_jgg9E',
+  authDomain: 'smart-shopper-4c6e4.firebaseapp.com',
+  projectId: 'smart-shopper-4c6e4',
+  storageBucket: 'smart-shopper-4c6e4.appspot.com',
+  messagingSenderId: '640903200234',
+  appId: '1:640903200234:web:5483d6ec1e12b4f7ed0693',
 };
 
 export const app = initializeApp(firebaseConfig);
